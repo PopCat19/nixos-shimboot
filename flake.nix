befd9f213f0f8e82bcbf45c93c68d76b0548c00b
PopCat19 <atsuo11111@gmail.com>
2025-08-06 23:11:42 -0400

    rm raw-efi config

--- flake.nix
+++ flake.nix
@@ -16,7 +16,6 @@
       system = "x86_64-linux";
       
       # Import all module outputs
-      rawEfiImageOutputs = import ./flake_modules/raw-efi-image.nix { inherit self nixpkgs nixos-generators; };
       rawImageOutputs = import ./flake_modules/raw-image.nix { inherit self nixpkgs nixos-generators; };
       systemConfigurationOutputs = import ./flake_modules/system-configuration.nix { inherit self nixpkgs; };
       developmentEnvironmentOutputs = import ./flake_modules/development-environment.nix { inherit self nixpkgs; };
@@ -27,7 +26,6 @@
       # Merge packages from all modules
       packages = {
         ${system} =
-          (rawEfiImageOutputs.packages.${system} or {}) //
           (rawImageOutputs.packages.${system} or {}) //
           (initramfsPatchingOutputs.packages.${system} or {}) //
           (chromeosSourcesOutputs.packages.${system} or {}) //
