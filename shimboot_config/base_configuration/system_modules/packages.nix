831e5ace5b74cc15aad4ed14430d4ac9e30c6c63
PopCat19 <atsuo11111@gmail.com>
2025-09-01 18:40:09 -0400

    fix packages.nix

--- shimboot_config/base_configuration/system_modules/packages.nix
+++ shimboot_config/base_configuration/system_modules/packages.nix
@@ -12,20 +12,21 @@
     fastfetch
     hwinfo
     fish
+  ];
 
   programs.fish = {
-  enable = true;
-  interactiveShellInit = ''
-function fish_greeting --description 'Shimboot greeting'
-  if type -q shimboot_greeter
-    shimboot_greeter
-  else
-    echo Welcome to NixOS Shimboot
-  end
-  if type -q setup_nixos
-    echo "Tip: run 'setup_nixos' to configure Wi-Fi, expand rootfs, and set up your flake."
-  end
-end
-'';
-};
+    enable = true;
+    interactiveShellInit = ''
+      function fish_greeting --description 'Shimboot greeting'
+        if type -q shimboot_greeter
+          shimboot_greeter
+        else
+          echo Welcome to NixOS Shimboot
+        end
+        if type -q setup_nixos
+          echo "Tip: run 'setup_nixos' to configure Wi-Fi, expand rootfs, and set up your flake."
+        end
+      end
+    '';
+  };
 }
