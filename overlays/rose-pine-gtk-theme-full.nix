94a9d379c3beff2fa68845dff89a08e256db3f91
PopCat19 <atsuo11111@gmail.com>
2025-10-29 09:17:04 -0400

    chore(rose-pine-gtk-theme-full): update sha256 hash

--- overlays/rose-pine-gtk-theme-full.nix
+++ overlays/rose-pine-gtk-theme-full.nix
@@ -18,7 +18,7 @@ in {
       owner = "Fausto-Korpsvart";
       repo = "Rose-Pine-GTK-Theme";
       rev = "main";
-      sha256 = "sha256-eeBuGvJKdv/puMg2FN3Ue52OU1LgipEte/OhRwIDDs8=";
+      sha256 = "sha256-3V2s7s6fEFIKysAGdf35gmiEO7LJc+QCbOluykKi7Ck=";
     };
 
     nativeBuildInputs = [
