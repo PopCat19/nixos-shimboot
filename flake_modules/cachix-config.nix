be6b54e1a294951be4c134ee2ea17a0fcd3e15d1
PopCat19 <atsuo11111@gmail.com>
2026-01-02 18:46:16 -0500

    fix(cachix): remove default NixOS cache configuration
    - Remove cache.nixos.org from extra-substituters
    - Remove cache.nixos.org-1 from extra-trusted-public-keys
    - Default NixOS cache is already configured globally
    - Reduces redundancy in Cachix configuration

--- flake_modules/cachix-config.nix
+++ flake_modules/cachix-config.nix
@@ -12,11 +12,9 @@ _: {
   # Cachix configuration for all builds
   nixConfig = {
     extra-substituters = [
-      "https://cache.nixos.org"
       "https://shimboot-systemd-nixos.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "shimboot-systemd-nixos.cachix.org-1:vCWmEtJq7hA2UOLN0s3njnGs9/EuX06kD7qOJMo2kAA="
     ];
   };
