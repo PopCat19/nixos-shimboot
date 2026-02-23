9d9fcfb9fcd0aebcf3cdfb015794a8fd35d3834c
PopCat19 <atsuo11111@gmail.com>
2026-02-22 23:14:30 -0500

    feat(packages): add user package configuration [untested]

---
+++ shimboot_config/profiles/nixos-popcat19/main_configuration/home/packages.nix
@@ -0,0 +1,17 @@
+# Packages Module
+#
+# Purpose: Import all package category modules
+# Dependencies: All packages/ submodules
+# Related: home.nix
+#
+# This module:
+# - Imports package category modules
+# - Provides centralized package management
+{ ... }:
+{
+  imports = [
+    ./packages/communication.nix
+    ./packages/media.nix
+    ./packages/utilities.nix
+  ];
+}
