e1a46de8bd7a4011e8b1d38ef454d83e132e198d
PopCat19 <atsuo11111@gmail.com>
2026-02-23 02:54:04 -0500

    feat(helpers): add runtime dependencies to script derivations

--- shimboot_config/base_configuration/system/helpers/helpers.nix
+++ shimboot_config/base_configuration/system/helpers/helpers.nix
@@ -1,19 +1,79 @@
 # Helpers Module
 #
-# Purpose: Provide system packages for helper scripts
+# Purpose: Provide system packages for helper scripts with dependencies
 # Dependencies: bash, ./*.sh files
 # Related: system modules
 #
 # This module:
-# - Installs helper scripts as system packages
+# - Installs helper scripts as system packages with runtime dependencies
 # - Scripts are standalone bash executables
 # - No fish dependency required
 { pkgs, ... }:
+let
+  inherit (pkgs) writeShellApplication lib;
+  inherit (lib) getExe;
+
+  # Core utilities used by multiple scripts
+  coreDeps = with pkgs; [
+    coreutils
+    util-linux
+    gnugrep
+    gawk
+    gnused
+    jq
+  ];
+
+  # expand_rootfs dependencies
+  expandRootfsDeps = coreDeps ++ (with pkgs; [
+    cloud-utils # growpart
+    cryptsetup
+    e2fsprogs # resize2fs
+    fdisk
+  ]);
+
+  # fix-steam-bwrap dependencies
+  fixSteamBwrapDeps = coreDeps ++ (with pkgs; [
+    findutils
+  ]);
+
+  # setup_nixos dependencies
+  setupNixosDeps = coreDeps ++ (with pkgs; [
+    git
+    networkmanager
+    findutils
+    procps
+  ]);
+
+  # setup_nixos_config dependencies
+  setupNixosConfigDeps = coreDeps ++ (with pkgs; [
+    nix
+  ]);
+
+in
 {
   environment.systemPackages = [
-    (pkgs.writeShellScriptBin "fix-steam-bwrap" (builtins.readFile ./fix-steam-bwrap.sh))
-    (pkgs.writeShellScriptBin "expand_rootfs" (builtins.readFile ./expand_rootfs.sh))
-    (pkgs.writeShellScriptBin "setup_nixos_config" (builtins.readFile ./setup_nixos_config.sh))
-    (pkgs.writeShellScriptBin "setup_nixos" (builtins.readFile ./setup_nixos.sh))
+    (writeShellApplication {
+      name = "fix-steam-bwrap";
+      runtimeInputs = fixSteamBwrapDeps;
+      text = builtins.readFile ./fix-steam-bwrap.sh;
+    })
+
+    (writeShellApplication {
+      name = "expand_rootfs";
+      runtimeInputs = expandRootfsDeps;
+      text = builtins.readFile ./expand_rootfs.sh;
+    })
+
+    (writeShellApplication {
+      name = "setup_nixos_config";
+      runtimeInputs = setupNixosConfigDeps;
+      text = builtins.readFile ./setup_nixos_config.sh;
+    })
+
+    (writeShellApplication {
+      name = "setup_nixos";
+      runtimeInputs = setupNixosDeps;
+      text = builtins.readFile ./setup_nixos.sh;
+    })
   ];
 }
