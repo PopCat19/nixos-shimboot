2b8547bae8f52ce058bfd497e1ba5d9843ded558
PopCat19 <atsuo11111@gmail.com>
2026-02-25 10:48:29 -0500

    fix(wallpaper): use relative path instead of env var

--- shimboot_config/main_configuration/home/wallpaper.nix
+++ shimboot_config/main_configuration/home/wallpaper.nix
@@ -6,17 +6,16 @@
 #
 # This module:
 # - Creates ~/Pictures/Wallpapers directory
-# - Soft-clones wallpapers from NIXOS_CONFIG_DIR (doesn't overwrite existing)
+# - Soft-clones wallpapers from config (doesn't overwrite existing)
 # - Allows users to add custom wallpapers without git tracking
 { config, lib, ... }:
 let
   wallpapersDir = "${config.home.homeDirectory}/Pictures/Wallpapers";
-  configWallpapers = "${builtins.getEnv "NIXOS_CONFIG_DIR"}/shimboot_config/main_configuration/home/wallpaper";
 in
 {
   home.activation.cloneWallpapers = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     mkdir -p ${wallpapersDir}
-    for file in ${configWallpapers}/*; do
+    for file in ${./wallpaper}/*; do
       filename=$(basename "$file")
       if [ ! -e "${wallpapersDir}/$filename" ]; then
         cp -r "$file" "${wallpapersDir}/"
