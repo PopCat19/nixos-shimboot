59cc93552c038bd4f203ad9d2595044ab2fb865c
PopCat19 <atsuo11111@gmail.com>
2026-03-03 22:50:48 -0500

    feat(nix): add cache.nixos.org as priority substituter and consolidate nix-options

--- shimboot_config/base_configuration/configuration.nix
+++ shimboot_config/base_configuration/configuration.nix
@@ -20,6 +20,7 @@
 }:
 {
   imports = [
+    ./nix-options.nix
     ./system/environment.nix
     ./system/boot.nix
     ./system/networking.nix
@@ -47,32 +48,6 @@
   ];
 
   _module.args.userConfig = userConfig;
-
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    trusted-users = lib.mkAfter [
-      "root"
-      "${userConfig.user.username}"
-    ];
-    substituters = lib.mkAfter [ "https://shimboot-systemd-nixos.cachix.org" ];
-    trusted-public-keys = lib.mkAfter [
-      "shimboot-systemd-nixos.cachix.org-1:vCWmEtJq7hA2UOLN0s3njnGs9/EuX06kD7qOJMo2kAA="
-    ];
-
-    # Enable store optimization at build time
-    auto-optimise-store = true; # Hardlink duplicate files
-    min-free = 0; # Don't reserve space
-  };
-
-  # Garbage collection: delete generations older than 30 days
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
-  };
 
   # Keep only 10 system generations before garbage collection runs
   systemd.services.nix-limit-generations = {
