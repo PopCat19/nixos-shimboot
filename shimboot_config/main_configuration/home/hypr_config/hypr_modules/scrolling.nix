22a71bce74866b02ee2328fcc4c96a75ca1bc360
PopCat19 <atsuo11111@gmail.com>
2026-03-11 09:34:28 -0400

    fix(hyprland): remove unsupported wrap options from scrolling

--- shimboot_config/main_configuration/home/hypr_config/hypr_modules/scrolling.nix
+++ shimboot_config/main_configuration/home/hypr_config/hypr_modules/scrolling.nix
@@ -17,8 +17,6 @@
       follow_focus = true;
       follow_min_visible = 0.4;
       explicit_column_widths = "0.333, 0.5, 0.667, 1.0";
-      wrap_focus = true;
-      wrap_swapcol = true;
       direction = "right";
     };
   };
