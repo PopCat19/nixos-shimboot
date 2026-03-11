02c4de194b7ecaa58fd15a3efa5592240cf1fc09
PopCat19 <atsuo11111@gmail.com>
2026-03-11 12:28:45 -0400

    fix(zram): add watermark tunings, lower page-cluster

--- shimboot_config/base_configuration/system/zram.nix
+++ shimboot_config/base_configuration/system/zram.nix
@@ -15,12 +15,12 @@ _: {
     enable = true;
     algorithm = "lzo-rle";
     memoryPercent = 60;
-    priority = 80;
+    priority = 100;
   };
 
   boot.kernel.sysctl = {
     "vm.swappiness" = 100;
-    "vm.page-cluster" = 0;
+    "vm.page-cluster" = 3;
     "vm.oom-kill" = 1;
     "vm.admin_reserve_kbytes" = 1024;
     "vm.overcommit_memory" = 1;
@@ -28,5 +28,7 @@ _: {
     "vm.vfs_cache_pressure" = 50;
     "vm.dirty_ratio" = 15;
     "vm.dirty_background_ratio" = 5;
+    "vm.watermark_boost_factor" = 6000;
+    "vm.watermark_scale_factor" = 125;
   };
 }
