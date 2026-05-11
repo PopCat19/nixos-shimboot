7ffd43e5a552093a355a07e3ff94467076599f50
PopCat19 <atsuo11111@gmail.com>
2026-05-10 22:34:17 -0400

    fix: disable nix-functional-tests via overlay (sandbox failures)

--- overlays/overlays.nix
+++ overlays/overlays.nix
@@ -5,4 +5,11 @@ _system: [
   # Import overlays
   # Rosé Pine full GTK theme (Main & Moon variants with icons)
   (import ./rose-pine-gtk-theme-full.nix)
+
+  # Nix: skip functional tests (fail on devices with sandboxing disabled)
+  (_final: _prev: {
+    nix = _prev.nix.overrideAttrs (_: {
+      doCheck = false;
+    });
+  })
 ]
